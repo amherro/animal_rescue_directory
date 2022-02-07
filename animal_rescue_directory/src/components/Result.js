57b77932e39734bd5fb03661eccaaffe898c4e16
Adam Herro <adam.herro@comcast.net>
2022-02-06 21:02:48 -0500

    Adding result info (address, phone, url) to the result card. NOTE: Will most likely make a modal to display these three fields, but wanted to have them on the card for now.

--- animal_rescue_directory/src/components/Result.js
+++ animal_rescue_directory/src/components/Result.js
@@ -4,8 +4,12 @@ const Result = () => {
     return (
         <div className='result-card'>
             <div className="main-info">
-                <h4 className="result-title">Puppies Inc.</h4>
-                <p className="result-distance">1.4 miles away</p>
+                <h4 className="result-info result-title">Puppies Inc.</h4>
+                <p className="result-info result-distance">1.4 miles away</p>
+                {/* Put these in modal that opens */}
+                <p className="result-info result-address">555 Main St</p>
+                <p className="result-info result-phone">555-123-4567</p>
+                <p className="result-info result-site">www.animalrescue.com</p> 
             </div>
            <button className="open-modal">More Info</button>
         </div>
@@ -13,8 +17,3 @@ const Result = () => {
 }
 
 export default Result
-
-//Put these in modal that opens
-{/* <p className="result-address">555 Main St</p>
-<p className="result-phone">555-123-4567</p>
-<p className="result-site">www.animalrescue.com</p> */}
