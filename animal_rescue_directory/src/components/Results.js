936c9a94c2fd4ca87866cb7f275c858e2c69b2d1
Adam Herro <adam.herro@comcast.net>
2022-02-21 18:07:30 -0500

    Adding state to Results component.

--- animal_rescue_directory/src/components/Results.js
+++ animal_rescue_directory/src/components/Results.js
@@ -1,8 +1,12 @@
+import {useState, useEffect} from 'react'
 import Result from "./Result"
 import '../styles/style.scss'
 
 
 const Results = () => {
+    const [results, setResults] = useState([]);
+
+
     return (
         <div className='results-section'>
             <Result />
